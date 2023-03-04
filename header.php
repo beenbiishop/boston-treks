b460e320e6052ada53bd7f015392d359d1eda21d
Ben Bishop <ben@benbishopsb.com>
2023-03-03 23:51:48 -0500

    Favicons

--- header.php
+++ header.php
@@ -7,14 +7,16 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link rel="apple-touch-icon" sizes="180x180"
 	      href="<?php echo get_template_directory_uri() . '/images/favicons/apple-touch-icon.png'; ?>">
-	<link rel="icon" type="image/png" sizes="32x32"
-	      href="<?php echo get_template_directory_uri() . '/images/favicons/favicon-32x32.png'; ?>">
-	<link rel="icon" type="image/png" sizes="16x16"
-	      href="<?php echo get_template_directory_uri() . '/images/favicons/favicon-16x16.png'; ?>">
+	<link rel="icon" type="image/svg+xml"
+	      href="<?php echo get_template_directory_uri() . '/images/favicons/favicon.svg'; ?>">
+	<link rel="icon" type="image/png"
+	      href="<?php echo get_template_directory_uri() . '/images/favicons/favicon.png'; ?>">
 	<link rel="manifest"
 	      href="<?php echo get_template_directory_uri() . '/images/favicons/site.webmanifest'; ?>">
 	<link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/images/favicons/safari-pinned-tab.svg'; ?>"
 	      color="#1e293b">
+	<meta name=”msapplication-config”
+	      content=”<?php echo get_template_directory_uri() . '/images/favicons/browserconfig.xml'; ?>”>
 	<meta name="msapplication-TileColor" content="#1e293b">
 	<meta name="theme-color" content="#1e293b">
 
