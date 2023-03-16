aeed9aedfeae2fa97a3fe13126d1937324123579
Ben Bishop <ben@benbishopsb.com>
2023-03-16 16:52:48 -0400

    Change stroke width

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
 	   class="<?php echo get_post_field( 'post_name', get_post() ) === 'discover' ? 'active' : ''; ?>">
 		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
 		     stroke="currentColor">
-			<path stroke-linecap="round" stroke-linejoin="round"
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
 			      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
 		</svg>
 
@@ -35,7 +35,7 @@
 	   class="<?php echo get_post_field( 'post_name', get_post() ) === 'profile' ? 'active' : ''; ?>">
 		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
 		     stroke="currentColor">
-			<path stroke-linecap="round" stroke-linejoin="round"
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
 			      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"/>
 		</svg>
 
