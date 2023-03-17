b05df39757db1449a40911924c011c99ce13df97
Ben Bishop <ben@benbishopsb.com>
2023-03-17 18:24:23 -0400

    Update Menus

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ function boston_treks_setup() {
 	add_theme_support( 'wp-block-styles' );
 
 	add_theme_support( 'editor-styles' );
-	add_editor_style( 'css/editor-style.css' );
+	add_editor_style( 'assets/css/editor-style.css' );
 }
 
 add_action( 'after_setup_theme', 'boston_treks_setup' );
