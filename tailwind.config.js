8bd8ee0f254dd44bd33228bd2a6ad643ed6fc633
Ben Bishop <ben@benbishopsb.com>
2023-03-23 13:44:33 -0400

    Remove Tailwind Forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,6 +58,6 @@ module.exports = {
 			]
 		}
 	}, plugins: [
-		require( '@tailwindcss/forms' ), require( "@tailwindcss/typography" ), require( "daisyui" ), tailpress.tailwind
+		require( "@tailwindcss/typography" ), require( "daisyui" ), tailpress.tailwind
 	]
 };
